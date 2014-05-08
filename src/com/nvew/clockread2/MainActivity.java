e4a58b8925eb7426b145474657d2d0b309ddedc9
Ed Watt <edwatt92@gmail.com>
2014-05-08 17:52:27 -0400

    Continued work on detecting intersections.

--- src/com/nvew/clockread2/MainActivity.java
+++ src/com/nvew/clockread2/MainActivity.java
@@ -334,7 +334,7 @@ public class MainActivity extends Activity  implements CvCameraViewListener2 {
 		   {
 			   root = SelfBalancingTree.insertIntoTree(root, endpts[n]);
 			   
-			   //if(doIntersect(endpts[n]))
+			   //if(doIntersect(endpts[n], root.pred(endpts[n]))
 			   
 		   }
 		   
@@ -389,6 +389,58 @@ public class MainActivity extends Activity  implements CvCameraViewListener2 {
 	   
 	   return dist <= max_dist;
    }
+   
+   public static boolean do_intersect(EndPoint a, EndPoint b)
+   {
+	   		Point p1 = a.self, q1 = a.other, p2 = b.self, q2 = b.other;
+	   
+	       // Find the four orientations needed for general and
+	       // special cases
+	       int o1 = orientation(p1, q1, p2);
+	       int o2 = orientation(p1, q1, q2);
+	       int o3 = orientation(p2, q2, p1);
+	       int o4 = orientation(p2, q2, q1);
+	    
+	       // General case
+	       if (o1 != o2 && o3 != o4)
+	           return true;
+	    
+	       // Special Cases
+	       // p1, q1 and p2 are colinear and p2 lies on segment p1q1
+	       if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+	    
+	       // p1, q1 and p2 are colinear and q2 lies on segment p1q1
+	       if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+	    
+	       // p2, q2 and p1 are colinear and p1 lies on segment p2q2
+	       if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+	    
+	        // p2, q2 and q1 are colinear and q1 lies on segment p2q2
+	       if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+	    
+	       return false; // Doesn't fall in any of the above cases
+	   }
+
+	public static int orientation(Point p, Point q, Point r)
+	{
+	    // See 10th slides from following link for derivation of the formula
+	    // http://www.dcs.gla.ac.uk/~pat/52233/slides/Geometry1x1.pdf
+	    int val = (int) ((q.y - p.y) * (r.x - q.x) -
+	              (q.x - p.x) * (r.y - q.y));
+	 
+	    if (val == 0) return 0;  // colinear
+	 
+	    return (val > 0)? 1: 2; // clock or counterclock wise
+	}
+	
+	public static boolean onSegment(Point p, Point q, Point r)
+	{
+	    if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+	        q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
+	       return true;
+	 
+	    return false;
+	}
    
    
    public Mat overlay_target(Mat mRgba){
